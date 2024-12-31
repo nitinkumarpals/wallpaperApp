d0013792b64838af0bbe9d0081a5eea62b365d16
nitinkumarpals <nitinpal900@gmail.com>
2024-12-31 13:17:06 +0530

    Added Stack Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,18 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function Layout() {
-    return (
-        <GestureHandlerRootView>
-            <Slot />
-        </GestureHandlerRootView>
-    )
+  return (
+    <GestureHandlerRootView>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="(nobottomtab)/accountInfo"
+          options={{
+            headerShown: true,
+            headerTitle: "Account Info",
+            headerBackTitle: "Back",
+          }}
+        />
+      </Stack>
+    </GestureHandlerRootView>
+  );
 }
